3b64977942a3be5a44055227b5081eed4d29b586
Mathilde-P <mathilde.picque@gmail.com>
2023-07-27 01:58:25 +0200

    fix: :bug: add link for inscription
    PA-25

--- petsitter-frontend/src/app/connexion/page.tsx
+++ petsitter-frontend/src/app/connexion/page.tsx
@@ -15,28 +15,36 @@ export default function Connexion() {
             <Image src={img_form} alt={""} />
           </div>
           <div className="form">
-            <Form className="test">
-              <h1>Connexion</h1>
-              <Form.Group
-                className="input"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label>Adresse mail</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
-              </Form.Group>
-              <Form.Group className="input" controlId="formPlaintextPassword">
-                <Form.Label column sm="2">
-                  Mot de passe
-                </Form.Label>
-                <Form.Control type="password" placeholder="Mot de passe" />
-                <span className="forget_mdp">
-                  Mot de passe oubliée ? <Link href={"#"}>Cliquez ici</Link>
-                </span>
-              </Form.Group>
-              <Button variant="primary" className="btn-connection">
-                Se connecter
-              </Button>
-            </Form>
+            <Container>
+              <Form className="form-inputs">
+                <div className="title-form">
+                  <h1>Connexion</h1>
+                  <span>
+                    Vous ne possédez pas de compte ?{" "}
+                    <Link href={"#"}>Inscrivez-vous ici</Link>
+                  </span>
+                </div>
+                <Form.Group
+                  className="input"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label>Adresse mail</Form.Label>
+                  <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group className="input" controlId="formPlaintextPassword">
+                  <Form.Label column sm="2">
+                    Mot de passe
+                  </Form.Label>
+                  <Form.Control type="password" placeholder="Mot de passe" />
+                  <span className="forget_mdp">
+                    Mot de passe oubliée ? <Link href={"#"}>Cliquez ici</Link>
+                  </span>
+                </Form.Group>
+                <Button variant="primary" className="btn-connection">
+                  Se connecter
+                </Button>
+              </Form>
+            </Container>
           </div>
         </div>
       </section>
